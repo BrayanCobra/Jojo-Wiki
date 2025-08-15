b070b0224a16a16cf8b6cd23f9487dff23e71dd8
BrayanCobra <brayancobrafr@gmail.com>
2025-08-15 00:50:21 -0300

    Primeiro commit do projeto

---
+++ scritps.js
@@ -0,0 +1,13 @@
+const kira1 = document.querySelector(".jonathanimgk")
+
+function cliqueiNoBotao(){
+    kira1.src = "img/kira1.png"
+}
+
+function cliqueiDeNovo(){
+    kira1.src = "img/kira2.png"
+}
+
+function cliquei3(){
+    kira1.src = "img/kira3.png"
+}
